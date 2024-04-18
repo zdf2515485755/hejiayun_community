164091a57c7da582ae190b3d84994f48f863580a
mrzhang <2515485755@qq.com>
2024-04-19 02:15:01 +0800

    feature:Implement-preloading-function

--- hejiayun-master/internal-common/src/main/java/com/zdf/internalcommon/constant/HjyCommunityConstant.java
+++ hejiayun-master/internal-common/src/main/java/com/zdf/internalcommon/constant/HjyCommunityConstant.java
@@ -5,7 +5,9 @@ package com.zdf.internalcommon.constant;
  *@Author mrzhang
  *@Date 2024/4/3 01:58
  */
-public class HjyCommunityConstant extends CommonConstant{
+public class HjyCommunityConstant{
     public static final String COMMUNITY_CODE_PREFIX = "COMMUNITY_";
     public static final String UPDATE_INSERT_AUTHOR = "admin";
+
+    private HjyCommunityConstant(){}
 }
